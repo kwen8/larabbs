89996ad2c942129c0173b44f99b5580e84f3022a
chankwen <418415881@qq.com>
2017-11-20 09:46:09 +0800

    Topic order

--- app/Http/Controllers/CategoriesController.php
+++ app/Http/Controllers/CategoriesController.php
@@ -7,9 +7,12 @@ use Illuminate\Http\Request;
 use App\Models\Category;
 class CategoriesController extends Controller
 {
-    public function show(Category $category)
+    public function show(Request $request, Category $category)
     {
-        $topics = Topic::where('category_id', $category->id)->paginate(20);
+        $topics = Topic::with('category', 'user')
+            ->where('category_id', $category->id)
+            ->withOrder($request->order)
+            ->paginate(20);
 
         return view('topics.index', compact('topics', 'category'));
     }
