cf74d6198e34dbeb596b1c0c2f536b2dd5a0a82e
chankwen <418415881@qq.com>
2017-11-22 21:05:16 +0800

    Topic detail page
    Signed-off-by: chankwen <418415881@qq.com>

--- app/Http/Controllers/TopicsController.php
+++ app/Http/Controllers/TopicsController.php
@@ -40,15 +40,13 @@ class TopicsController extends Controller
 		$topic->user_id = Auth::id();
 		$topic->save();
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Created successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('message', '成功创建话题.');
 	}
 
 	public function edit(Topic $topic)
 	{
         $this->authorize('update', $topic);
-        $category = $topic->category();
-        $categories = Category::all();
-		return view('topics.create_and_edit', compact('topic', 'category', 'categories'));
+		return view('topics.create_and_edit', compact('topic'));
 	}
 
 	public function update(TopicRequest $request, Topic $topic)
@@ -56,7 +54,7 @@ class TopicsController extends Controller
 		$this->authorize('update', $topic);
 		$topic->update($request->all());
 
-		return redirect()->route('topics.show', $topic->id)->with('message', 'Updated successfully.');
+		return redirect()->route('topics.show', $topic->id)->with('message', '更新成功.');
 	}
 
 	public function destroy(Topic $topic)
@@ -64,7 +62,7 @@ class TopicsController extends Controller
 		$this->authorize('destroy', $topic);
 		$topic->delete();
 
-		return redirect()->route('topics.index')->with('message', 'Deleted successfully.');
+		return redirect()->route('topics.index')->with('message', '删除成功.');
 	}
 
     public function uploadImage(Request $request, ImageUploadHandler $uploader)
